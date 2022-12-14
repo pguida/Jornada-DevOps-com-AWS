6e6e42eed6bd3361477c6e2a07c07f3fc8d45098
Propercio Gurgel Guida Junior <propercioggj@gmail.com>
2022-12-14 10:49:13 -0300

    Create docker.sh

---
+++ Docker/vagrant/docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install Docker
+apt-get update && upgrade -y
+apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release nfs-common
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null  
+  
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
