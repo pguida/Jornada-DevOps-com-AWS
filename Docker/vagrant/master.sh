ac07afbf4c6a6c12a3a7911574855f4d80185d6e
Propercio Gurgel Guida Junior <propercioggj@gmail.com>
2022-12-14 11:00:47 -0300

    Update master.sh

--- Docker/vagrant/master.sh
+++ Docker/vagrant/master.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker swarm init --advertise-addr=192.168.68.100
+sudo docker swarm init --advertise-addr=192.168.68.10
 sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
